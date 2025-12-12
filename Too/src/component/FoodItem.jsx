89e2391822d6c32d0bf1a8b216bb8dcb59acc1a8
naveen shukla <naveenshukla2501@gmail.com>
2025-12-12 17:44:22 +0530

    Change image source to dynamic URL
    Updated image source to use dynamic URL from context.

--- Too/src/component/FoodItem.jsx
+++ Too/src/component/FoodItem.jsx
@@ -3,12 +3,12 @@ import { assets } from '../frontend_assets/assets'
 import { StoreContext } from '../Context/StoreContext';
 
 const FoodItem = ({id,name,description,price,image}) => {
-    const {cartItem ,addToCart,removeFromCart } = useContext(StoreContext);
+    const {cartItem ,addToCart,removeFromCart,url } = useContext(StoreContext);
 
   return (
     <div className='p-1 bg-white  shadow-md rounded-lg '>
       <div className='relative '>
-        <img src={"http://localhost:4000/image/"+image} className=''  />
+        <img src={url+"/image/"+image} className=''  />
         { !cartItem?.[id] 
             ?<img onClick={()=>addToCart(id)} src={assets.add_icon_white} className='w-[2rem] absolute bottom-2 right-2 cursor-pointer'/>
             :<div className='absolute bottom-2 right-2 flex items-center gap-2 p-2 rounded-2xl bg-white'>
@@ -29,4 +29,5 @@ const FoodItem = ({id,name,description,price,image}) => {
 }
 
 export default FoodItem
+
 
